0f77bef0585ee6de0e56e2471123b0b152d9d0b3
JJineu <96639305+JJineu@users.noreply.github.com>
2022-08-26 03:53:00 +0900

    [Silver III] Title: 문자열 집합, Time: 160 ms, Memory: 36012 KB -BaekjoonHub

--- 백준/Silver/14425. 문자열 집합/문자열 집합.py
+++ 백준/Silver/14425. 문자열 집합/문자열 집합.py
@@ -1,14 +1,17 @@
 import sys
 input = sys.stdin.readline
 
-n, m = map(int, input().split())
-s = set()
-for i in range(n):
-    s.add(input().rstrip())
-    
-cnt = 0
-for i in range(m):
-    check = input().rstrip()
-    if check in s:
-        cnt += 1
-print(cnt)
+def main():
+    n, m = map(int, input().split())
+    s = set()
+    for i in range(n):
+        s.add(input().rstrip())
+        
+    cnt = 0
+    for i in range(m):
+        check = input().rstrip()
+        if check in s:
+            cnt += 1
+    print(cnt)
+
+main()
