ca6fb4685a669abf43d90d7a3762e7e1249301db
JJineu <96639305+JJineu@users.noreply.github.com>
2022-08-25 20:46:36 +0900

    [Silver V] Title: 회사에 있는 사람, Time: 240 ms, Memory: 43644 KB -BaekjoonHub

--- 백준/Silver/7785. 회사에 있는 사람/회사에 있는 사람.py
+++ 백준/Silver/7785. 회사에 있는 사람/회사에 있는 사람.py
@@ -9,7 +9,7 @@ for i in range(n):
         h[name] = 1
     else:
         del h[name]
-sorted_h = sorted(h.items(), key=lambda item: item[0], reverse=True)
+sorted_h = sorted(h.keys(), reverse=True)
 # print(sorted_h)
 for i in range(len(sorted_h)):
-    print(sorted_h[i][0])    
+    print(sorted_h[i])    
