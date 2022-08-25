137117614a6918ee172e004069dd483c931dea4e
JJineu <96639305+JJineu@users.noreply.github.com>
2022-08-26 04:30:53 +0900

    [Silver IV] Title: 카드, Time: 164 ms, Memory: 53908 KB -BaekjoonHub

---
+++ 백준/Silver/11652. 카드/카드.py
@@ -0,0 +1,15 @@
+import sys
+input = sys.stdin.readline
+
+def main():
+    n = int(input().rstrip())
+    dic = {}
+    for i in range(n):
+        tmp = int(input())
+        if tmp in dic:
+            dic[tmp] += 1
+        else:
+            dic[tmp] = 1
+    dic = sorted(dic.items(), key=lambda x : (-x[1],x[0]))
+    print(dic[0][0])
+main()
