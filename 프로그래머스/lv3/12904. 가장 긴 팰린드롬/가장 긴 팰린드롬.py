58334c2851b0f0d8d076aa6eb1ab206d33dd9116
JJineu <96639305+JJineu@users.noreply.github.com>
2022-08-26 18:17:01 +0900

    [level 3] Title: 가장 긴 팰린드롬, Time: 558.28 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/lv3/12904. 가장 긴 팰린드롬/가장 긴 팰린드롬.py
+++ 프로그래머스/lv3/12904. 가장 긴 팰린드롬/가장 긴 팰린드롬.py
@@ -21,15 +21,38 @@
                     
 #     return answer
 
-def solution(s):
-    answer = 1
-    l = len(s)
+
+# def solution(s):
+#     answer = 1
+#     l = len(s)
  
-    for start in range(l):
-        for end in range(start + 2, l + 1):
-            a = s[start:end]
-            if len(a) < answer:
-                continue
-            if a == a[::-1]:
-                answer = max(answer, end - start)
-    return answer
+#     for start in range(l):
+#         for end in range(start + 2, l + 1):
+#             a = s[start:end]
+#             if len(a) < answer:
+#                 continue
+#             if a == a[::-1]:
+#                 answer = max(answer, end - start)
+#     return answer
+
+def solution(s):
+    lbound=0
+    rbound=0
+    maxlength=0
+
+    while lbound<len(s) and rbound<len(s):
+        lb = lbound
+        rb = rbound
+        while lb>=0 and rb<len(s):
+            if s[lb]==s[rb]:
+                maxlength = max(maxlength, rb-lb+1)
+                # print("lb = %d, rb = %d" % (lb, rb))
+                lb -=1
+                rb +=1
+            else:
+                break
+        if lbound == rbound:
+            rbound += 1
+        else:
+            lbound += 1
+    return maxlength
